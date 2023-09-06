aa29222e4728680ab01970f8b0167ea6d2474274
Vera0712 <yutongvera@gmail.com>
2023-09-06 13:10:59 -0400

    Create an interface of Shape with a void method getArea. For all the specific shapes, implementing the Shape interface and create its own getArea method, and also put the fields private. For square, extending with rectangle.

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,7 @@
 import { newRenderer } from "./renderer.js"
 import { Rectangle} from "./shapes/rectangle.js";
+import { Shape } from "./shapes/shape.js";
 
-const rectangle: Rectangle = new Rectangle(2, 3)
+const rectangle: Shape = new Rectangle(2, 3)
 const renderer = newRenderer(rectangle)
 renderer.draw();
